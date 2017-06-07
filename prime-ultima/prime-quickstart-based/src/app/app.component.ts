990ef9dc21615ec2c7e2f1da3b16397bc7641bee
sai <saikiran.mandhala@gmail.com>
2017-06-07 07:25:50 +0530

    Added basic theming validation code to primeng ultima
    Added basic theming validation code to primeng ultima

--- prime-ultima/prime-quickstart-based/src/app/app.component.ts
+++ prime-ultima/prime-quickstart-based/src/app/app.component.ts
@@ -2,17 +2,29 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  moduleId: module.id,
-  selector: 'my-app',
-  templateUrl: 'app.component.html'
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
 })
-export class AppComponent { 
-    
-    name: string; 
-    
+export class AppComponent {
+
+    name: string;
+
     message: string;
-    
+
     onClick() {
         this.message = 'Hello ' + this.name;
+    }
+    changeTheme(event: Event, theme: string) {
+        let layOutLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-layout');
+        let themeLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-css');
+        if (theme == 'cyan') {
+            layOutLink.href = '/layout-purple-cyan.css';//assets/components/themes/' + theme + '/theme.css';
+            themeLink.href = '/theme-purple-cyan.css';//'assets/components/themes/' + theme + '/theme.css';
+        } else {
+            layOutLink.href = '/layout-blue.css';//assets/components/themes/' + theme + '/theme.css';
+            themeLink.href = '/theme-blue.css';//'assets/components/themes/' + theme + '/theme.css';
+        }
+        event.preventDefault();
     }
 }
